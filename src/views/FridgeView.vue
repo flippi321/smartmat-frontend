f340e8726b84983d1fcff10e7c168824a6030899
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-19 23:04:48 +0200

    Modified FridgeView to have multiple text fields, to better visualise changes to Header and background

--- src/views/FridgeView.vue
+++ src/views/FridgeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>This is a fridge</h1>
+    <div v-for="index in 20" :key="index">
+      <h1>This is a fridge</h1>
+    </div>
   </div>
 </template>
 
@@ -10,6 +13,12 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+  }
+
+  .about h1 {
+    margin-top: 50px; /* Temporary, only to show background and Header functionality */
   }
 }
 </style>
+
