cab7110efa5f922ded834aa795577fec66e5db34
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-20 18:28:00 +0200

    Added component for the sidebar used in the fridge to filter content

---
+++ src/components/fridgePage/fridgeSidebarComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  categories: {
+    type: [],
+    required: true,
+  }
+});
+</script>
+
+<template>
+  <h1>Filter By Category:</h1>
+  <ul>
+    <li v-for="category in categories" :key="category.id" @click="changeCategory(category.id)">
+      {{ category.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToItem(itemId) {
+      this.$router.push({ path: `/fridgeItem`, query: { itemId } });
+    },
+    changeCategory(categoryId) {
+      this.$router.push({ path: `/fridge`, query: { id: this.id, category: categoryId } });
+    },
+  },
+}
+</script>
