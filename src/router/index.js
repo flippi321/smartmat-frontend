dd6d4d0e0e9279cc935151d3187d7af83d263940
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-21 09:54:34 +0200

    Updated Index.js to have a more unified language

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import FridgeView from "@/views/FridgeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import HouseholdView from "@/views/HouseholdView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,19 +32,18 @@ const router = createRouter({
       path: '/fridge',
       name: 'fridge',
       props: route => ({ id: parseInt(route.query.id), category: parseInt(route.query.category) }),
-      component: () => import('../views/FridgeView.vue')
+      component: FridgeView
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     },
     {
       path: '/household',
       name: 'household',
       props: route => ({ id: parseInt(route.query.id) }),
-      component: () =>
-          import("../views/HouseholdView.vue"),
+      component: HouseholdView,
     }
   ]
 })
