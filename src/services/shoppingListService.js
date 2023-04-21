d212df085447da1e50411e367b52d7b2c45aa8de
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-21 14:37:58 +0200

    Added a shoppingListService which handles API calls and mocks responses (Will be changed to actual responses later)

---
+++ src/services/shoppingListService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+//let url = 'https://localhost:8000'
+
+/*
+Mocking responses, must be removed:
+*/
+import MockAdapter from "axios-mock-adapter";
+var mock = new MockAdapter(axios);
+import groceryItems from "@/mockDatabases/grocery-items.json";
+import sortingChoices from "@/mockDatabases/sorting-choices.json"
+
+// Mock any GET request to /fridgeFromId
+mock.onGet("/getItemsFromShoppingList", { params: { id: 1, sortBy: 1 } }).reply(200, {
+    groceryItems,
+});
+
+mock.onGet("/getSortingChoices").reply(200, {
+    sortingChoices,
+})
+
+export default {
+    getShoppingListContents(listId, sortBy){
+        console.log("ID-er:");
+        console.log(listId)
+        console.log(sortBy)
+        return(axios.get("/getItemsFromShoppingList", { params: { id: listId, sortBy: sortBy } }));
+    },
+    getSortingChoices(){
+        return(axios.get("/getSortingChoices"));
+    }
+}
