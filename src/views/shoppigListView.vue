3cc33231a28b1196b5a464240c7bba37ffd6f965
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-22 15:58:22 +0200

    Added a shoppingListView which combines the shoppingListSortbar and shoppingListContents

---
+++ src/views/shoppigListView.vue
@@ -0,0 +1,71 @@
+<script setup>
+// Import Components
+import Groceries from "@/components/shoppingListPage/shoppingListContents.vue"
+import FilterBar from "@/components/shoppingListPage/shoppingListSortbar.vue";
+
+// Define Props
+defineProps({
+  id: {
+    type: Number,
+    required: true
+  },
+  sortBy:{
+    type: Number,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <div class="shopping-list-page">
+    <div class="sidebar">
+      <FilterBar :sortChoices="sortingChoices" :list-id="id"></FilterBar>
+    </div>
+    <div class="groceries-container">
+      <Groceries :items="items"></Groceries>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import shoppingListService from "@/services/shoppingListService";
+
+export default {
+  data() {
+    return {
+      id: this.id,
+      items: [],
+      sortingChoices: [],
+    };
+  },
+  created() {
+    shoppingListService.getShoppingListContents(this.id, this.sortBy).then(response => {
+      console.log(response.data.groceryItems.groceries)
+      this.items = response.data.groceryItems.groceries;
+    });
+
+    shoppingListService.getSortingChoices().then(response => {
+      console.log(response.data.sortingChoices)
+      this.sortingChoices = response.data.sortingChoices.sortingChoices;
+    });
+  }
+};
+</script>
+
+<style>
+.sidebar {
+  position: fixed;
+  margin-top: 60px;
+  height: 100%;
+  width: 400px;
+  background-color: white;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+}
+
+.groceries-container {
+  margin-left: 400px;
+  padding: 10px;
+}
+</style>
