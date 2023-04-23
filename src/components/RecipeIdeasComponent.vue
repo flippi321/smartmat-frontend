36e403fa99b47e53a6c59adea8e854c70ad8d33c
teobirkeland <teobir32@gmail.com>
2023-04-23 12:48:40 +0200

    Added a scroll button to the recipe suggestions and made it fit different screen sizes

--- src/components/RecipeIdeasComponent.vue
+++ src/components/RecipeIdeasComponent.vue
@@ -9,24 +9,27 @@ defineProps({
 
 <template>
     <div class="box-container">
-        <h1 id="header">Middagsforslag</h1>
-        <div id="recipe-container">
+        <h1 class="header">Middagsforslag</h1>
+        <div class="recipe-container">
             <div class="input-container">
-                <label id="nrOfPeopleLabel" for="nrOfPeople">Antall personer:</label>
+                <label class="nrOfPeopleLabel" for="nrOfPeople">Antall personer:</label>
                 <input type="number" id="nrOfPeople" v-model="nrOfPeople">
             </div>
-            <div id="recipeList" class="grid-container">
-                <a class="grid-item" v-for="recipe in recipes" :key="recipe.id" :href="'/recipes/' + recipe.id">
-                    <h3 id="recipeName">{{ recipe.name }}</h3>
-                    <img id="recipeImg" src="https://images.unsplash.com/photo-1615870216519-2f9fa575fa5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80"
-                         alt="alternatetext" width="200">
-                    <p id="recipeDescription">{{ recipe.description }}</p>
-                </a>
+            <div class="scroll-container">
+                <div class="recipeList grid-container">
+                    <a class="grid-item" v-for="recipe in recipes" :key="recipe.id" :href="'/recipes/' + recipe.id">
+                        <h3 class="recipeName">{{ recipe.name }}</h3>
+                        <img class="recipeImg" src="https://images.unsplash.com/photo-1615870216519-2f9fa575fa5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80"
+                             alt="alternatetext" width="200">
+                        <p class="recipeDescription">{{ recipe.description }}</p>
+                        <p class="nrOfIngredients">4/8 ingredienser</p>
+                    </a>
+                </div>
+                <button class="scroll-button" @click="scrollRight">››</button>
             </div>
         </div>
     </div>
 </template>
-
 
 
 <script>
@@ -39,6 +42,32 @@ export default {
             recipes: [],
             nrOfPeople: 0,
         };
+    },
+    methods: {
+        scrollRight() {
+            const gridContainer = this.$el.querySelector('.grid-container');
+            const scrollWidth = gridContainer.scrollWidth;
+            const currentScroll = gridContainer.scrollLeft;
+
+            // Calculate the remaining scroll width
+            const remainingScroll = scrollWidth - currentScroll - gridContainer.clientWidth;
+
+            // If the remaining scroll width is less than the width of a grid-item plus margin,
+            // scroll to the beginning, otherwise scroll by the width of a grid-item plus margin
+            if (remainingScroll < 320) {
+                gridContainer.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            } else {
+                gridContainer.scrollBy({
+                    top: 0,
+                    left: 320,
+                    behavior: 'smooth'
+                });
+            }
+        },
     },
     created() {
         recipeService.getRecipes(1).then(response => {
@@ -53,29 +82,39 @@ export default {
         });
     },
 };
-
 </script>
 
+
 <style>
-body{
+body {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    font-family: sans-serif;
     margin-top: 100px;
 }
 
 .box-container {
-    max-width: 800px;
-    margin: 0 auto;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
 }
 
-#recipe-container {
+.recipe-container {
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
 }
 
-#header {
-    font-size: 32px;
+.header {
+    font-size: 2.5vw;
     margin-bottom: 40px;
     color: black;
+    text-align: center;
 }
 
 .input-container {
@@ -85,8 +124,8 @@ body{
     margin-bottom: 30px;
 }
 
-#nrOfPeopleLabel {
-    font-size: 24px;
+.nrOfPeopleLabel {
+    font-size: 1.5vw;
     margin-right: 10px;
 }
 
@@ -96,7 +135,7 @@ body{
     border-radius: 5px;
     border: none;
     padding: 0 10px;
-    font-size: 16px;
+    font-size: 1vw;
 }
 
 .grid-container {
@@ -114,7 +153,8 @@ body{
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     transition: transform 0.2s ease-in-out;
     flex: 0 0 auto;
-    width: 300px;
+    width: 80vw;
+    max-width: 300px;
     margin-right: 20px;
 }
 
@@ -127,24 +167,111 @@ body{
     box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
 }
 
-#recipeName {
-    font-size: 24px;
+.recipeName {
+    font-size: 1.8vw;
     margin-bottom: 10px;
     color: black;
 }
 
-#recipeImg {
+.recipeImg {
     border-radius: 5px;
     margin-bottom: 10px;
     width: 100%;
 }
 
-#recipeDescription {
-    font-size: 16px;
+.recipeDescription {
+    font-size: 1vw;
     color: #666;
+    margin-bottom: 5px;
+}
+
+.nrOfIngredients {
+    font-size: 0.8vw;
+    color: #999;
+}
+
+.scroll-container {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+}
+
+.scroll-button {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    background: white;
+    border: 1px solid #d1d1d1;
+    font-size: 24px;
+    padding: 5px 10px;
+    border-radius: 50%;
+    cursor: pointer;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    transition: all 0.2s ease-in-out;
+    outline: none;
+    color: #333;
+}.scroll-button:hover {
+     transform: translateY(-50%) scale(1.1);
+     box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+     background-color: #f8f8f8;
+ }
+
+.scroll-button:active {
+    transform: translateY(-50%) scale(0.95);
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
+}
+
+/* Media queries for responsiveness */
+@media (max-width: 768px) {
+    .header {
+        font-size: 4vw;
+    }
+
+    .nrOfPeopleLabel {
+        font-size: 2.5vw;
+    }
+
+    #nrOfPeople {
+        font-size: 2vw;
+    }
+
+    .recipeName {
+        font-size: 3vw;
+    }
+
+    .recipeDescription {
+        font-size: 2vw;
+    }
+
+    .nrOfIngredients {
+        font-size: 1.5vw;
+    }
+}
+
+@media (max-width: 480px) {
+    .header {
+        font-size: 6vw;
+    }
+
+    .nrOfPeopleLabel {
+        font-size: 4vw;
+    }
+
+    #nrOfPeople {
+        font-size: 3vw;
+    }
+
+    .recipeName {
+        font-size: 5vw;
+    }
+
+    .recipeDescription {
+        font-size: 3vw;
+    }
+
+    .nrOfIngredients {
+        font-size: 2vw;
+    }
 }
 </style>
-
-
-
-
