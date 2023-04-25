c6f6c6506ff5f5abad67abfaf0e5cb6f753a35e9
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-25 17:35:26 +0200

    Added prompts holding the user information to the UserPageComponent

--- src/components/UserPageComponent.vue
+++ src/components/UserPageComponent.vue
@@ -1,18 +1,30 @@
 <script setup>
 defineProps({
-  user: {
-    type: Object,
+  id: {
+    type: Number,
     required: true
-  }
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
 })
 </script>
 
 <template>
   <div>
     <h1>User Information</h1>
-    <p><strong>User ID:</strong> {{ user.data.userId }}</p>
-    <p><strong>First Name:</strong> {{ user.data.firstName }}</p>
-    <p><strong>Last Name:</strong> {{ user.data.lastName }}</p>
-    <p><strong>Email:</strong> {{ user.data.email }}</p>
+    <p><strong>User ID:</strong> {{ id }}</p>
+    <p><strong>First Name:</strong> {{ firstName }}</p>
+    <p><strong>Last Name:</strong> {{ lastName }}</p>
+    <p><strong>Email:</strong> {{ email }}</p>
   </div>
 </template>
