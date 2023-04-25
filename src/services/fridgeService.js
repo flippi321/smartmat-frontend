80a081c71d946fe605706cb0f68d04f8a3a49c1d
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-25 13:11:29 +0200

    Fixed service class so that it now mocks a response from API instead of returning a JSON file automatically

--- src/services/fridgeService.js
+++ src/services/fridgeService.js
@@ -1,16 +1,17 @@
 import axios from "axios";
+const apiClient = axios.create();
 //let url = 'https://localhost:8000'
 
 /*
 Mocking responses, must be removed:
 */
 import MockAdapter from "axios-mock-adapter";
-const mock = new MockAdapter(axios);
+const mock = new MockAdapter(apiClient);
 import groceryItems from "@/mockDatabases/grocery-items.json";
 import categories from "@/mockDatabases/categories.json"
 
 // Mock any GET request to /fridgeFromId
-mock.onGet("/getItemsFromFridge").reply(200, {
+mock.onGet("/getItemsFromFridge", { params: { id: 1 } }).reply(200, {
     groceryItems,
 });
 
@@ -21,17 +22,9 @@ mock.onGet("/getFridgeCategories").reply(200, {
 
 export default {
     getFridgeContents(fridgeId){
-        console.log(fridgeId)
-        /*
-        return(axios.get("/getItemsFromFridge"));
-         */
-        return(groceryItems);
+        return apiClient.get("/getItemsFromFridge", { params: { id: fridgeId } });
     },
-    getCategoriesFromFridgeId(fridgeId){
-        console.log(fridgeId)
-        /*
-        return(axios.get("/getFridgeCategories"));
-         */
-        return(categories);
-    }
+    getCategoriesFromFridgeId(){
+        return apiClient.get("/getFridgeCategories");
+    },
 }
