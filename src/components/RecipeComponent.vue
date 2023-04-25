80683c457adea3663aa1ce04361a7962f1008c80
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-25 10:18:14 +0200

    Added a basic RecipeComponent

---
+++ src/components/RecipeComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="recipe-box">
+    <h2>{{ recipe.name }}</h2>
+    <p>{{ recipe.description }}</p>
+    <h3>Ingredienser</h3>
+    <ul>
+      <li v-for="(ingredient, index) in recipe.ingredients" :key="index">
+        {{ ingredient.amount }} {{ ingredient.unit }} {{ ingredient.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import recipeService from "@/services/recipeService";
+
+export default {
+  data() {
+    return {
+      recipe: {}
+    };
+  },
+  methods: {
+    getRecipe(id) {
+      recipeService.getRecipieById(id).then(response => {
+        this.recipe = response.data;
+      });
+    }
+  },
+  mounted() {
+    this.getRecipe(1); // Replace 1 with the actual recipe ID
+  }
+};
+</script>
