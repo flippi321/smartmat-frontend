9d652ad8c7b5b956fc0c9f662c190f7b02b7a77f
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-25 17:19:07 +0200

    Added a simple component showing information about the user

---
+++ src/components/UserPageComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  user: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>User Information</h1>
+    <p><strong>User ID:</strong> {{ user.data.userId }}</p>
+    <p><strong>First Name:</strong> {{ user.data.firstName }}</p>
+    <p><strong>Last Name:</strong> {{ user.data.lastName }}</p>
+    <p><strong>Email:</strong> {{ user.data.email }}</p>
+  </div>
+</template>
