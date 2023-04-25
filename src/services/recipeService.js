c7c44f0e47a9090c5eaa4a3784a2da44ef1d9af1
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-25 09:17:47 +0200

    Added mock call to fetch the recipe stored in recipe-item.json

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -8,14 +8,23 @@ import MockAdapter from "axios-mock-adapter";
 var mock = new MockAdapter(axios);
 
 import recipes from "@/mockDatabases/recipes.json";
+import recipeItem from "@/mockDatabases/recipe-item.json";
 
 mock.onGet("/getRecipes", { params: { id: 1 } }).reply(200, {
     recipes,
 });
 
+mock.onGet("/getRecipes", { params: { id: 1 } }).reply(200, {
+    recipeItem,
+});
+
 export default {
     getRecipes(householdId){
         return(axios.get("/getRecipes", { params: { id: householdId } }));
+    },
+
+    getRecipieById(recipieId){
+        return(axios.get("/getRecipieById", { params: {id: recipieId} } ));
     }
 }
 
