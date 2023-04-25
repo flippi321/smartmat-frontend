51229c8b2957c790f0985bf10e76a3faa52526ea
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-25 17:35:05 +0200

    Made UserView import imformation found in the store

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,9 +1,16 @@
 <script setup>
 import UserPageComponent from "@/components/UserPageComponent.vue";
+import { useAuthStore } from "@/stores";
+
+const authStore = useAuthStore();
 </script>
 
 <template>
   <div>
-    <UserPageComponent/>
+    <UserPageComponent
+        :id="authStore.getFirstName"
+        :first-name="authStore.getLastName"
+        :last-name="authStore.getEmail"
+        :email="authStore.getUserId"/>
   </div>
 </template>
