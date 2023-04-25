936fc456016ee2ae6c08c9440fac79bee27a259d
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-25 10:57:07 +0200

    Fixed issue where mock-adapter would not correctly mock responses

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -1,31 +1,45 @@
 import axios from "axios";
+const apiClient = axios.create();
 //let url = 'https://localhost:8000'
+
 
 /*
 Mocking responses, must be removed:
 */
 import MockAdapter from "axios-mock-adapter";
-var mock = new MockAdapter(axios);
-
 import recipes from "@/mockDatabases/recipes.json";
 import recipeItem from "@/mockDatabases/recipe-item.json";
 
-mock.onGet("/getRecipes", { params: { id: 1 } }).reply(200, {
-    recipes,
+// Set up the mock adapter with the new axios instance
+const mock = new MockAdapter(apiClient);
+
+mock.onGet("/getRecipes").reply(config => {
+    const { id } = config.params;
+    if (id === 1) {
+        return [200, recipes];
+    } else {
+        return [400, { message: "Household not found" }];
+    }
 });
 
-mock.onGet("/getRecipes", { params: { id: 1 } }).reply(200, {
-    recipeItem,
+mock.onGet("/getRecipeById").reply(config => {
+    const { id } = config.params;
+    if (id === 1) {
+        return [200, recipeItem];
+    } else {
+        return [404, { message: "Recipe not found" }];
+    }
 });
 
+/*
+Sending API Calls to server
+ */
 export default {
-    getRecipes(householdId){
-        return(axios.get("/getRecipes", { params: { id: householdId } }));
+    getRecipes(householdId) {
+        return apiClient.get("/getRecipes", { params: { id: householdId } });
     },
 
-    getRecipieById(recipieId){
-        return(axios.get("/getRecipieById", { params: {id: recipieId} } ));
-    }
-}
-
-
+    getRecipeById(recipeId) {
+        return apiClient.get("/getRecipeById", { params: { id: recipeId } });
+    },
+};
