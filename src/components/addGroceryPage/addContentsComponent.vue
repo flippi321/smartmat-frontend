53744f84a61ec2bf5c20106a04c398d6fc4b0b43
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-26 17:18:06 +0200

    Added a script and style to the grid showing addable grocery items

--- src/components/addGroceryPage/addContentsComponent.vue
+++ src/components/addGroceryPage/addContentsComponent.vue
@@ -36,75 +36,115 @@ defineProps({
   </div>
 </template>
 
-  <style>
+<script>
+import {ref} from "vue";
+
+export default {
+  data() {
+    return {
+      expandedItem: ref(null),
+    }
+  },
+
+  methods: {
+    seeDetails(item) {
+      if(this.expandedItem === null){
+        this.expandedItem = item
+        this.$emit("hide-filter-bar")
+        console.log("Hide sidebar")
+      }
+    },
+
+    showSidebar(){
+      console.log("Show sidebar")
+      this.$emit("show-filter-bar");
+      this.expandedItem = null;
+    },
+
+    addGrocery(groceryItem){
+      this.showSidebar()
+      this.$emit("add-grocery", groceryItem)
+      this.expandedItem = null;
+    }
+  }
+}
+</script>
+
+<style>
+.fridge-box-container {
+  margin-left: 50px;
+  position: relative;
+  left: 0;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 15px;
+}
+
+.fridge-box {
+  background-color: white;
+  height: 35vh;
+  width: 90%;
+  transition: all 0.3s;
+  border-radius: 10px;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.fridge-item-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.fridge-item-info {
+  font-size: 16px;
+  text-align: center;
+  position: absolute;
+  bottom: 5%;
+  left: 50%;
+  transform: translateX(-50%);
+  transition: all 0.3s;
+  background-color: gray;
+}
+
+.fridge-box.expanded {
+  position: absolute;
+  left: 15vh;
+  height: 70vh;
+  width: 70vw;
+  z-index: 100;
+}
+
+.fridge-box.hidden {
+  display: none;
+}
+
+.details-container {
+  height: 0;
+  width: 0;
+  transition: all 0.3s;
+}
+
+@media screen and (max-width: 768px) {
   .fridge-box-container {
-    margin-left: 50px;
-    position: relative;
-    left: 0;
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 15px;
+    grid-template-columns: repeat(2, 1fr);
+    margin-left: 0;
+    padding: 20px;
   }
 
   .fridge-box {
-    background-color: white;
-    height: 35vh;
-    width: 90%;
-    transition: all 0.3s;
-    border-radius: 10px;
-    position: relative;
-    overflow: hidden;
-    cursor: pointer;
-  }
-
-  .fridge-item-image {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  .fridge-item-info {
-    font-size: 16px;
-    text-align: center;
-    position: absolute;
-    bottom: 5%;
-    left: 50%;
-    transform: translateX(-50%);
-    transition: all 0.3s;
-    background-color: gray;
+    height: 40vw;
+    width: 40vw;
   }
 
   .fridge-box.expanded {
-    position: absolute;
-    left: 15vh;
-    height: 70vh;
-    width: 70vw;
-    z-index: 100;
+    height: 90%;
+    width: 90%;
   }
-
-  .fridge-box.hidden {
-    display: none;
-  }
-
-  @media screen and (max-width: 768px) {
-    .fridge-box-container {
-      grid-template-columns: repeat(2, 1fr);
-      margin-left: 0;
-      padding: 20px;
-    }
-
-    .fridge-box {
-      height: 40vw;
-      width: 40vw;
-    }
-
-    .fridge-box.expanded {
-      height: 90%;
-      width: 90%;
-    }
-  }
-  </style>
+}
+</style>
