d6c56836517957f260bd77a66dd3e03ccd24f90c
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-26 18:42:02 +0200

    Updated the image src value to contain "@" instead of "src"

--- src/components/fridgePage/fridgeContentsComponent.vue
+++ src/components/fridgePage/fridgeContentsComponent.vue
@@ -49,7 +49,7 @@ function handleFeedback1(feedbackInfo){
 <template>
   <div class="fridge-box-container">
     <button @click="emit('add-new-items')" class="add-items-button">
-      Legg til varer <img src="src/assets/icons/plusSign.png" alt="Add" class="plus-icon" />
+      Legg til varer <img src="@/assets/icons/plusSign.png" alt="Add" class="plus-icon" />
     </button>
     <div
         class="fridge-box"
