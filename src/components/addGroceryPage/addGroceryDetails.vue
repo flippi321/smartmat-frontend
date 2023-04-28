d92463aaeda343761aa96799094f6b632015db1f
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-04-28 09:39:48 +0200

    Removed button in favor of the universal button style

--- src/components/addGroceryPage/addGroceryDetails.vue
+++ src/components/addGroceryPage/addGroceryDetails.vue
@@ -98,21 +98,6 @@ export default {
   margin: 0;
 }
 
-button {
-  background-color: #4CAF50;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 8px;
-  transition-duration: 0.4s;
-}
-
 button:hover {
   background-color: #555555;
   color: white;
