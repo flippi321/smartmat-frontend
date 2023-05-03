4708fdbfdb8c846f82e239fa3e340839b0d3bc4e
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-05-03 09:52:09 +0200

    Will now update the contents of the shopping list after closing the ad page

--- src/views/shoppingListView.vue
+++ src/views/shoppingListView.vue
@@ -105,6 +105,7 @@ export default {
 
         hideGroceryDetailComponent() {
             this.showAddGroceries = false;
+            this.updateShoppingList();
         },
 
         handleAddGrocery(items){
