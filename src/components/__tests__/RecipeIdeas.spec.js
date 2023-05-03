96dc43b7e3b1962036f398807f33ee1c0e441876
teobirkeland <teobir32@gmail.com>
2023-05-03 21:42:36 +0200

    Added correct mocking to test class that failed

--- src/components/__tests__/RecipeIdeas.spec.js
+++ src/components/__tests__/RecipeIdeas.spec.js
@@ -1,74 +1,116 @@
 import { mount } from '@vue/test-utils';
 import RecipeIdeasComponent from '@/components/RecipeIdeasComponent.vue';
-import recipeService from '@/services/recipeService';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import sinon from 'sinon';
-
-// Mock recipeService.getRecipes
-recipeService.getRecipes = async () => ({
-    data: [
-        {
-            id: 1,
-            name: 'Test Recipe',
-            imageUrl: 'test-image-url',
-            description: 'Test description',
-        },
-    ],
-});
+import axios from 'axios';
 
 describe('RecipeIdeasComponent.vue', () => {
-    it('renders the component', async () => {
-        const wrapper = mount(RecipeIdeasComponent, {
-            props: {
-                id: 1,
-            },
+    let axiosStub;
+
+    beforeEach(() => {
+        axiosStub = sinon.stub(axios, 'request');
+        axiosStub.withArgs(sinon.match({ url: sinon.match(/\/api\/recipes\/recommender\/\d+/) })).resolves({
+            data: [
+                {
+                    id: 1,
+                    name: 'Test Recipe',
+                    imageUrl: 'test-image-url',
+                    description: 'Test description',
+                },
+            ],
         });
-
-        await wrapper.vm.$nextTick();
-
-        const headerElement = wrapper.find('.header');
-        const recipeNameElement = wrapper.find('.recipeName');
-
-        expect(headerElement.text()).toBe('Middagsforslag');
-        expect(recipeNameElement.text()).toBe('Test Recipe');
+        axiosStub.withArgs(sinon.match({ url: sinon.match(/\/api\/recipes\/missingIngredients\/\d+\/\d+/) })).resolves({
+            data: [
+                {
+                    "id": 1,
+                    "name": "Missing Ingredients of: Eggerøre",
+                    "description": "Missing items",
+                    "ingredients": []
+                },
+                {
+                    "id": 1,
+                    "name": "Eggerøre",
+                    "description": "En enkel og deilig frokostrett laget med egg, melk og smør.",
+                    "ingredients": [
+                        {
+                            "id": 1,
+                            "name": "Melk",
+                            "amount": 0,
+                            "unit": "L"
+                        },
+                        {
+                            "id": 2,
+                            "name": "Egg",
+                            "amount": 4,
+                            "unit": "stk"
+                        }
+                    ]
+                }
+            ],
+        });
     });
 
-    it('scrolls right', async () => {
-        const wrapper = mount(RecipeIdeasComponent, {
-            props: {
-                id: 1,
-            },
-        });
-
-        await wrapper.vm.$nextTick();
-
-        const gridContainer = wrapper.find('.grid-container').element;
-
-        if (typeof gridContainer.scrollTo === 'undefined') {
-            gridContainer.scrollTo = sinon.stub();
-        } else {
-            sinon.stub(gridContainer, 'scrollTo');
-        }
-
-        await wrapper.find('.scroll-button').trigger('click');
-        await wrapper.vm.$nextTick();
-
-        expect(gridContainer.scrollTo.called).toBeTruthy();
+    afterEach(() => {
+        axiosStub.restore();
     });
 
-    it('updates nrOfPeople when input changes', async () => {
-        const wrapper = mount(RecipeIdeasComponent, {
-            props: {
-                id: 1,
-            },
+    describe('RecipeIdeasComponent.vue', () => {
+        it('renders the component', async () => {
+            const wrapper = mount(RecipeIdeasComponent, {
+                props: {
+                    id: 1,
+                },
+            });
+
+            await wrapper.vm.$nextTick();
+
+            const headerElement = wrapper.find('.header');
+            const recipeNameElement = wrapper.find('.recipeName');
+
+            expect(headerElement.text()).toBe('Middagsforslag');
+            expect(recipeNameElement.text()).toBe('Test Recipe');
         });
 
-        await wrapper.vm.$nextTick();
+        it('scrolls right', async () => {
+            const wrapper = mount(RecipeIdeasComponent, {
+                props: {
+                    id: 1,
+                },
+            });
 
-        const input = wrapper.find('#nrOfPeople');
-        await input.setValue(3);
-        await wrapper.vm.$nextTick();
+            await wrapper.vm.$nextTick();
 
-        expect(wrapper.vm.nrOfPeople).toBe(3);
+            const gridContainer = wrapper.find('.grid-container').element;
+
+            if (typeof gridContainer.scrollTo === 'undefined') {
+                gridContainer.scrollTo = sinon.stub();
+            } else {
+                sinon.stub(gridContainer, 'scrollTo');
+            }
+
+            await wrapper.find('.scroll-button').trigger('click');
+            await wrapper.vm.$nextTick();
+
+            expect(gridContainer.scrollTo.called).toBeTruthy();
+        });
+
+        it('updates nrOfPeople when input changes', async () => {
+            const wrapper = mount(RecipeIdeasComponent, {
+                props: {
+                    id: 1,
+                },
+            });
+
+            await wrapper.vm.$nextTick();
+
+            const input = wrapper.find('#nrOfPeople');
+            await input.setValue(3);
+            await wrapper.vm.$nextTick();
+
+            expect(wrapper.vm.nrOfPeople).toBe(3);
+        });
     });
+
 });
+
+
