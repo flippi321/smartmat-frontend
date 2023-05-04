107b56135e1f742092b75f716f6b603ff926be83
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-05-04 23:25:49 +0200

    Made it so that a user with an active household automatically gets redirected to the households page

--- src/views/JoinHouseholdView.vue
+++ src/views/JoinHouseholdView.vue
@@ -1,36 +1,52 @@
 <template>
   <joinHouseholdComponent
       @createHousehold="createHouseHold"
-      @joinHouseHold="joinHousehold"
+      @joinHousehold="joinHousehold"
   />
 </template>
 
 <script>
 import joinHouseholdComponent from "@/components/JoinHouseholdComponent.vue"
 import householdService from "@/services/householdService";
+import {useAuthStore} from "@/stores";
+import router from "@/router";
 
 export default {
   name: "JoinHouseholdView",
+
+  data(){
+    return{
+      store: useAuthStore(),
+    }
+  },
 
   components:{
     joinHouseholdComponent,
   },
 
   methods: {
-    createHouseHold(userId){
-      householdService.createHousehold(userId).then(response => {
+    createHouseHold(){
+      householdService.createHousehold(this.store.getUserId).then(response => {
         console.log(response.data)
       }).catch(error => {
         console.log(error)
       })
     },
 
-    joinHousehold(userId, invitationNumber){
-      householdService.createHousehold(userId, invitationNumber).then(response => {
+    joinHousehold(invitationNumber){
+      householdService.createHousehold(this.store.getUserId, invitationNumber).then(response => {
         console.log(response.data)
       }).catch(error => {
         console.log(error)
       })
+    }
+  },
+
+  created() {
+    if(this.store.getHousehold === 0){
+      router.push({ path: '/household', query: { id: this.store.getHousehold } });
+    } else {
+      console.log(this.store.getHousehold)
     }
   }
 }
