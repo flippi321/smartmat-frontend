56f4878c3d2b5d2f839e0ec33de7453103e87474
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-05-04 22:20:21 +0200

    Made the JoinHouseholdComponent able to create a household with the users Id

--- src/views/JoinHouseholdView.vue
+++ src/views/JoinHouseholdView.vue
@@ -4,14 +4,23 @@
 
 <script>
 import joinHouseholdComponent from "@/components/joinHouseholdComponent.vue"
+import householdService from "@/services/householdService";
+
 export default {
   name: "JoinHouseholdView",
+
   components:{
     joinHouseholdComponent,
+  },
+
+  methods: {
+    createAHouseHold(userId){
+      householdService.createHousehold(userId).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
