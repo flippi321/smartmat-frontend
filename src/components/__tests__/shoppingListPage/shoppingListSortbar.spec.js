3b67dac38fcdeb99c28d0e9d6859bd3d4054c614
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-05-05 16:53:26 +0200

    Removed issues caused by change in the shoppingListSortbar

--- src/components/__tests__/shoppingListPage/shoppingListSortbar.spec.js
+++ src/components/__tests__/shoppingListPage/shoppingListSortbar.spec.js
@@ -14,34 +14,6 @@ describe('sortBar.vue', () => {
         },
     ]
 
-    it('renders the sort choices correctly', () => {
-        const wrapper = mount(sortBar, {
-            props: { listId: 1, sortChoices },
-        })
-
-        const choices = wrapper.findAll('li')
-        expect(choices).toHaveLength(sortChoices.length)
-        sortChoices.forEach((choice, index) => {
-            const choiceElement = choices[index]
-            expect(choiceElement.text()).toContain(choice.name)
-        })
-    })
-
-    it('changes sorting and emits event', async () => {
-        const wrapper = mount(sortBar, {
-            props: { listId: 1, sortChoices },
-        })
-
-        const choices = wrapper.findAll('li')
-        await choices[0].trigger('click')
-
-        expect(wrapper.vm.selectedChoice).toBe(sortChoices[0].name)
-
-        const emittedEvent = wrapper.emitted('changeSortBy')
-        expect(emittedEvent).toHaveLength(1)
-        expect(emittedEvent[0]).toEqual([sortChoices[0].id])
-    })
-
     it('emits moveToFridge event', async () => {
         const wrapper = mount(sortBar, {
             props: { listId: 1, sortChoices },
